e73c2de1b8f4277bfb0bc7fdec514941c0b105bc
MikhailT <tymkaev@gmail.com>
2025-02-22 14:39:15 +1300

    add gitignore

--- AutoDocApi/Database/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using AutoDocApi.Models;
-
-namespace AutoDocApi.Database;
-
-public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<TodoTask> TodoTasks { get; set; }
-}
