6aeb82b21c82900b03eb359cd0ab89d69be3ae23
MikhailT <tymkaev@gmail.com>
2025-02-23 19:33:14 +1300

    update git ignore, add put, delete

--- AutoDocApi/Contract/CreateTodoTaskRequest.cs
+++ AutoDocApi/Contract/CreateTodoTaskRequest.cs
@@ -2,6 +2,13 @@ namespace AutoDocApi.Contract;
 
 public class CreateTodoTaskRequest
 {
-    public required string Title { get; set; }
+    public string Title { get; set; }= default!;
     public DateTime DueDate { get; set; }
+}
+
+public class UpdateTodoTaskRequest
+{
+    public string Title { get; set; } = default!;
+    public DateTime DueDate { get; set; }
+    public string Status { get; set; } = default!;
 }
--- AutoDocApi/Endpoints/TasksEndpoints.cs
+++ AutoDocApi/Endpoints/TasksEndpoints.cs
@@ -9,7 +9,7 @@ namespace AutoDocApi.Endpoints
     {
         public static void MapTasksEndpoints(this IEndpointRouteBuilder app)
         {
-            app.MapPost("/todotask", async (
+            app.MapPost("/todotasks", async (
                 CreateTodoTaskRequest createTodoTaskRequest,
                 AppDbContext dbContext,
                 CancellationToken cancellationToken
@@ -29,7 +29,27 @@ namespace AutoDocApi.Endpoints
                 return Results.Created($"/todotask/{todoTask!.Id}", todoTask);
             });
 
-            app.MapGet("/todotask", async (
+            app.MapGet("/todotasks", async (
+                string status,
+                int? page,
+                int? pageSize,
+                AppDbContext context,
+                CancellationToken cancellationToken) =>
+                {
+                    var todoTask = await context.TodoTasks
+                    .AsNoTracking()
+                    .Where(t => t.Status == status)
+                    .ToListAsync(cancellationToken: cancellationToken);
+
+                    if (todoTask is null)
+                    {
+                        return Results.NoContent();
+                    }
+
+                    return Results.Ok(todoTask);
+                });
+
+            app.MapGet("/todotasks/{id}", async (
                 string id,
                 AppDbContext context,
                 CancellationToken cancellationToken) =>
@@ -41,7 +61,7 @@ namespace AutoDocApi.Endpoints
 
                     var todoTask = await context.TodoTasks
                     .AsNoTracking()
-                    .FirstOrDefaultAsync(t=> t.Id == guid, cancellationToken: cancellationToken);
+                    .FirstOrDefaultAsync(t => t.Id == guid, cancellationToken: cancellationToken);
 
                     if (todoTask is null)
                     {
@@ -51,27 +71,39 @@ namespace AutoDocApi.Endpoints
                     return Results.Ok(todoTask);
                 });
 
-            app.MapGet("/todotask/{id}", async (
-                string id,
-                AppDbContext context,
-                CancellationToken cancellationToken) =>
-                {
-                    if (!Guid.TryParse(id, out var guid))
+            app.MapPut("/todotasks/{id}", async (string id, UpdateTodoTaskRequest dto, AppDbContext context) =>
+            {
+                if (!Guid.TryParse(id, out var guid))
                     {
                         return Results.BadRequest();
                     }
 
-                    var todoTask = await context.TodoTasks
+                var todoTask = await context.TodoTasks
                     .AsNoTracking()
-                    .FirstOrDefaultAsync(t=> t.Id == guid, cancellationToken: cancellationToken);
+                    .FirstOrDefaultAsync(t => t.Id == guid);
 
-                    if (todoTask is null)
-                    {
-                        return Results.NotFound();
-                    }
+                if (todoTask is null) return Results.NotFound();
 
-                    return Results.Ok(todoTask);
-                });
+                todoTask.Title = dto.Title;
+                todoTask.DueDate = dto.DueDate;
+                todoTask.Status = dto.Status;
+
+                await context.SaveChangesAsync();
+
+                return Results.NoContent();
+            });
+
+            app.MapDelete("/todotasks/{id}", async (int id, AppDbContext context) =>
+            {
+                if (await context.TodoTasks.FindAsync(id) is TodoTask todoTask)
+                {
+                    context.TodoTasks.Remove(todoTask);
+                    await context.SaveChangesAsync();
+                    return Results.Ok();
+                }
+
+                return Results.NotFound();
+            });
         }
     }
 }
